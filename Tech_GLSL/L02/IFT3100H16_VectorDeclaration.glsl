ddb0ecfa607e102ea5175450642b0fd6cf3a5d3d
philvoyer <philvoyer@gmail.com>
2016-02-25 11:49:47 -0500

    publication
    leçon 2

---
+++ Tech_GLSL/L02/IFT3100H16_VectorDeclaration.glsl
@@ -0,0 +1,14 @@
+// IFT3100H16_VectorDeclaration.glsl
+// Exemples de déclaration de variables de type vecteur en GLSL.
+
+// déclaration d'une variable scalaire à virgule flottante de une dimension
+float f = 1.0;
+
+// déclaration d'une variable de type vecteur avec deux dimensions
+vec2 vector2D = vec2(1.0, 2.0);
+
+// déclaration d'une variable de type vecteur avec trois dimensions
+vec3 vector3D = vec3(1.0, 2.0, 3.0);
+
+// déclaration d'une variable de type vecteur avec quatre dimensions
+vec4 vector4D = vec4(1.0, 2.0, 3.0, 4.0);
