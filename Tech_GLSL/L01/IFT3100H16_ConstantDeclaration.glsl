ed766070d94e99f3b313233d4ac65ed0cf65a420
philvoyer <philvoyer@gmail.com>
2016-02-25 11:49:17 -0500

    publication
    leçon 1

---
+++ Tech_GLSL/L01/IFT3100H16_ConstantDeclaration.glsl
@@ -0,0 +1,19 @@
+// IFT3100H16_VectorDeclaration.glsl
+// Exemples de déclaration de variables constantes en GLSL.
+
+// constantes scalaires
+const float zero = 0.0;
+const float one  = 1.0;
+const float pi   = 3.14159;
+const float phi  = 1.61803;
+
+// constantes de couleur
+const vec4 black = vect4(0.0, 0.0, 0.0, 1.0);
+const vec4 white = vect4(1.0, 1.0, 1.0, 1.0);
+const vec4 grey  = vect4(0.5, 0.5, 0.5, 1.0);
+const vec4 red   = vect4(1.0, 0.0, 0.0, 1.0);
+const vec4 green = vect4(0.0, 1.0, 0.0, 1.0);
+const vec4 blue  = vect4(0.0, 0.0, 1.0, 1.0);
+
+// constante de transformation
+const mat4 identity = mat4(1.0);
