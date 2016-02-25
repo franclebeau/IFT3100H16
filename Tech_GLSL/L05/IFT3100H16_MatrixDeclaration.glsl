d62a30b5abafcae47c50e154bdca3900be4a52f5
philvoyer <philvoyer@gmail.com>
2016-02-25 11:50:27 -0500

    publication
    leçon 5

---
+++ Tech_GLSL/L05/IFT3100H16_MatrixDeclaration.glsl
@@ -0,0 +1,19 @@
+// IFT3100H16_MatrixDeclaration.glsl
+// Exemples de déclaration de variables de type matrice en GLSL.
+
+// matrice identité de dimension 3 par 3
+mat3 ma33 = mat3(1.0, 0.0, 0.0,  // colonne #1
+				 0.0, 1.0, 0.0,  // colonne #2
+				 0.0, 0.0, 1.0); // colonne #3
+
+// matrice identité de dimension 3 par 3
+mat3 mb33(1.0);
+
+// matrice identité de dimension 4 par 4
+mat4 ma44 = mat4(1.0, 0.0, 0.0, 0.0,  // colonne #1
+				 0.0, 1.0, 0.0, 0.0,  // colonne #2
+				 0.0, 0.0, 1.0, 0.0,  // colonne #3
+				 0.0, 0.0, 0.0, 1.0); // colonne #4
+
+// matrice identité de dimension 4 par 4
+mat4 mb44(1.0);
