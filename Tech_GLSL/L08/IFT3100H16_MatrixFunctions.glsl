c5362962c4eb99420b9fe3697de51e12829fb3d1
philvoyer <philvoyer@gmail.com>
2016-02-25 11:51:03 -0500

    publication
    leçon 8

---
+++ Tech_GLSL/L08/IFT3100H16_MatrixFunctions.glsl
@@ -0,0 +1,17 @@
+// IFT3100H16_MatrixFunctions.glsl
+// Exemples de fonctions applicables aux variables de type matrice en GLSL.
+
+mat4 m;
+
+// m = ...
+
+mat4 result;
+
+// transposition
+result = transpose(m);
+
+// inversion
+result = inverse(m);
+
+// determinant
+result = determinant(m);
